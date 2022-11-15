c2c56f8fdea0e9b6020eda06020469f7052dda41
aronez <aronez@e3r10p8.clusters.42paris.fr>
2022-11-15 10:15:15 +0100

    Fixed formatting and some leaks when mallocs fail.

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -7,8 +7,8 @@ int	main() {
 	void *p1 = 0, *p2 = (void *)1010;
 	int count = ft_printf("Hello %s!%%\n%c\n%x\n%d\n%u\n%p\n%p\n%c\n", "world", 'l', -154, -154, -154, p1, p2, 0);
 	ft_printf("Count : %d\n\n\n", count);
-	count = printf("Hello %s!%%\n%c\n%x\n%d\n%u\n%p\n%p\n%c\n", "world", 'l', -154, -154, -154, p1, p2, 0);
-	printf("Count : %d\n", count);
+	//count = printf("Hello %s!%%\n%c\n%x\n%d\n%u\n%p\n%p\n%c\n", "world", 'l', -154, -154, -154, p1, p2, 0);
+	//printf("Count : %d\n", count);
 
 	return (EXIT_SUCCESS);
 }
